7bad31876048c90bd668d518954a4b6b8e9f33d3
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-17 16:20:45 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,10 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_table :students do |t|
+        t.string "name"
+        t.boolean "vegetable"
+        t.integer "pizza_id"
+    end
+  end
 end
